637eac526be7873568ef864e358ec7a1ca8dce81
noguti0406 <izabely.martinelli.souza@escola.pr.gov.br>
2024-08-15 12:32:37 -0300

    adicionando jscss

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
